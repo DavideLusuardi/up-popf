adcd01bc31403f34b3762ec65360b90841d494b8
Davide Lusuardi <davide.lusuardi@studenti.unitn.it>
2022-11-18 11:55:29 +0100

    Added correct supported features, added gitignore

--- build/lib/up_popf/__init__.py
+++
@@ -1,1 +0,0 @@
-from .popf import popfPDDLPlanner
--- up_popf/plan.py
+++ up_popf/plan.py
@@ -4,7 +4,7 @@ import subprocess
 
 def main():
     if len(sys.argv) != 4:
-        exit(1) # TODO
+        exit(0)
     
     domain_filename, problem_filename, plan_filename = sys.argv[1:4]
     popf_executable = os.path.join(os.environ.get('POPF_HOME'), 'compile', 'popf2', 'popf3-clp')
@@ -13,21 +13,15 @@ def main():
         cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE
     )
 
-    try:
-        out_err_bytes = process.communicate()
-        proc_out, proc_err = [[x.decode()] for x in out_err_bytes]
-        proc_out = ''.join(proc_out)
-        proc_err = ''.join(proc_err)
-    except subprocess.TimeoutExpired:
-        exit(1) # TODO
+    out_err_bytes = process.communicate()
+    proc_out, proc_err = [[x.decode()] for x in out_err_bytes]
+    proc_out = ''.join(proc_out)
+    proc_err = ''.join(proc_err)
     retval = process.returncode
-
-    # print(f"retval: {retval}")
-    # print(f"proc_err: {proc_err}")
-    # print(f"proc_out: {proc_out}")
+    if retval != 0 or ';;;; Solution Found\n' not in proc_out:
+        exit(retval)
 
     plan = proc_out.split(';;;; Solution Found\n')[-1]
-    # print(plan)
 
     with open(plan_filename, "w") as f:
         f.write(plan)
